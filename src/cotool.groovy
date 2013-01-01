2d1f4dcb99657980e6943efd3508c05d73a58603
rdeman <yawalu01>
2013-01-01 19:50:38 +0100

    aaa

--- src/cotool.groovy
+++ src/cotool.groovy
@@ -1,18 +1,20 @@
-import groovy.time.*
-
 // ---------------- parametres
 // dossier contenant les fichiers
 path = 'D:\\dev\\groovy\\cotool\\'
+
 // fichier résultats
 inputFileName = 'result_test.csv'
 colonne_premiere_balise=7
+
 // fichier des equipes
 teamFileName = 'Inscrits2012.csv'
 
 // balises et penalités
+epreuves=[CO:'imposé',VTT:'libre']
+
 penalite1 = new Penalite("0:30:00")
 penalite2 = new Penalite("00:40:00")
-epreuves=[CO:'imposé',VTT:'libre']
+
 balises=[:]
 balises['CO']=[1:penalite1,2:penalite1,3:penalite1,4:penalite1,5:penalite2]
 balises['VTT']=[6:penalite2,7:penalite1,8:penalite1,9:penalite1,3:penalite2,10:penalite2]
